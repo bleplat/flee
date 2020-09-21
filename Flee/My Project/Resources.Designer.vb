ee7fdad7577a5176d6259bdb2ed23ce0a1a9e2ed
User <me@example.com>
2020-09-21 14:50:56 +0200

    changed 'attack only enemies' icon

--- Flee/My Project/Resources.Designer.vb
+++ Flee/My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property DeadSkull() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("DeadSkull", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Fissile() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Fissile", resourceCulture)
